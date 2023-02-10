43029b4419773958137e6f1a2ba4b8f1ef9e3432
Aiaminm <70017178+Aiaminm@users.noreply.github.com>
2023-02-10 21:00:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 'use strict';
-
+const cors = require('cors');
 const express = require('express');
+const timeout = require('connect-timeout');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
+const app = express();
+app.use(cors());
+
+const TIME_OUT = "60s";
+app.use(timeout(TIME_OUT));
+app.use((req, res, next) => {
+  if (!req.timedout) next();
+});
+
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
+
+app.use('/',createProxyMiddleware({ target: "https://ipfs.io", changeOrigin: true })
+
 
 app.listen(PORT, HOST);
+
+
+
